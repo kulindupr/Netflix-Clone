acb493c949c5bd1e8c53e908aed94c1826dc16f3
Kulindu Pabasara <148193086+kulindupr@users.noreply.github.com>
2024-05-18 22:59:47 +0530

    implement scroll bar

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './TitleCards.css'
+import cards_data from '../../assets/cards/Cards_data.js'
 
 const TitleCards = () => {
-    return (
-        <div className='titleCards'>
+    const cardsRef = useRef();
 
+    const handleWheel = (event) => {
+        event.preventDefault();
+        cardsRef.current.scrollLeft += event.deltaY;
+    }
+
+    useEffect(() => {
+        const currentRef = cardsRef.current;
+        currentRef.addEventListener('wheel', handleWheel);
+    }, []);
+
+    return (
+        <div className='title-cards'>
+            <h2>Popular on Netflix</h2>
+            <div className="card-list" ref={cardsRef}>
+                {cards_data.map((card, index) => {
+                    return (
+                        <div className="card" key={index}>
+                            <img src={card.image} alt="" />
+                            <p>{card.name}</p>
+                        </div>
+                    );
+                })}
+            </div>
         </div>
-    )
+    );
 }
 
-export default TitleCards
+export default TitleCards;
